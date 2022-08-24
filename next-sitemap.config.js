1a2bcb176b691687e54f3f4dd34fd96a8a5043ee
Sayan Mukherjee <hello@sayanmukherjee.com>
2022-08-24 08:44:21 +0530

    added nextjs sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//   siteUrl: "https://www.revovideo.com",
+//   generateRobotsTxt: true,
+//   sitemapSize: 7000,
+// };
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.revovideo.com",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
