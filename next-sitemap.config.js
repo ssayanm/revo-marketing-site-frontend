07e9cdc67e8ca81099fd0ae65e79a2b3da7bcc28
Sayan Mukherjee <hello@sayanmukherjee.com>
2022-09-06 15:34:51 +0530

    updated sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,9 +4,28 @@
 //   sitemapSize: 7000,
 // };
 
-/** @type {import('next-sitemap').IConfig} */
+// module.exports = {
+//   siteUrl: "https://revovideo.com",
+//   generateRobotsTxt: true, // (optional)
+//   sitemapSize: 7000,
+// };
+
+const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
 module.exports = {
-  siteUrl: "https://www.revovideo.com",
-  generateRobotsTxt: true, // (optional)
-  sitemapSize: 7000,
+  siteUrl,
+  exclude: ["/404"],
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        disallow: ["/404"],
+      },
+      { userAgent: "*", allow: "/" },
+    ],
+    additionalSitemaps: [
+      `${siteUrl}sitemap.xml`,
+      `${siteUrl}server-sitemap.xml`,
+    ],
+  },
 };
